a67ac1da2bfad7c47ef3490fdadcdc3dea8bf434
Guan Huang <ghuang2013@fau.edu>
2015-07-09 01:53:45 -0700

    refactoring

--- happyHourFinder/js/effect_main.js
+++ happyHourFinder/js/effect_main.js
@@ -1,6 +1,11 @@
 $(document).ready(function(){
     
     $('.pager').hide();
+    
+    $in = $('#in');
+    $up = $('#up');
+    $userForm = $('#userForm');
+    
     /*
         user sign-up/log-in form 
         switch bet. nav tabs
@@ -9,16 +14,16 @@ $(document).ready(function(){
     $('.navform').load('inc/form.php?method=Login&class=navbar-form');
     
     $('#userForm').load('inc/form.php?method=Login');
-    $('#in').on('click',function(){
-        $('#up').removeClass('active');
-        $('#in').addClass('active');
-        $('#userForm').load('inc/form.php?method=Login');
+    $in.on('click',function(){
+        $up.removeClass('active');
+        $in.addClass('active');
+        $userForm.load('inc/form.php?method=Login');
     });
     
-    $('#up').on('click',function(){
-        $('#up').addClass('active');
-        $('#in').removeClass('active');
-        $('#userForm').load('inc/form.php?method=SignUp');
+    $up.on('click',function(){
+        $up.addClass('active');
+        $in.removeClass('active');
+        $userForm.load('inc/form.php?method=SignUp');
     });
     /*
         result hover effects
